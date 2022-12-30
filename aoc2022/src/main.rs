1678f0f28d927acc21cffbb05e5e279a1cf41958
Isak Stenström <31575837+isakstenstrom@users.noreply.github.com>
2022-12-30 16:48:08 +0100

    Improved main runner function

--- aoc2022/src/main.rs
+++ aoc2022/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 mod day1;
 mod day19;
 mod day2;
@@ -9,28 +11,81 @@ mod day7;
 mod day9;
 mod util;
 
-fn main() {
-    let day: String = std::env::args().nth(1).unwrap();
-
-    match day.as_str() {
-        "1_1" => println!("{}", day1::task1()),
-        "1_2" => println!("{}", day1::task2()),
-        "2_1" => println!("{}", day2::task1()),
-        "2_2" => println!("{}", day2::task2()),
-        "3_1" => println!("{}", day3::task1()),
-        "3_2" => println!("{}", day3::task2()),
-        "4_1" => println!("{}", day4::task1()),
-        "4_2" => println!("{}", day4::task2()),
-        "5_1" => println!("{}", day5::task1()),
-        "5_2" => println!("{}", day5::task2()),
-        "6_1" => println!("{}", day6::task1()),
-        "6_2" => println!("{}", day6::task2()),
-        "7_1" => println!("{}", day7::task1()),
-        "7_2" => println!("{}", day7::task2()),
-        "9_1" => println!("{}", day9::task1()),
-        "9_2" => println!("{}", day9::task2()),
-        "19_1" => println!("{}", day19::task1()),
-        "19_2" => println!("{}", day19::task2()),
-        _ => println!("Day not implemented yet."),
+macro_rules! run_day {
+    ( $i:literal ,$run_all:ident, $day:ident, $task:ident ) => {
+        if $run_all || $day == $i {
+            if $task == -1 || $task == 1 {
+                let task_time = Instant::now();
+                println!(
+                    "Day {:>2}: Task 1: {:>16}, in {:?}",
+                    $i,
+                    paste::expr! {[<day $i>]::task1()},
+                    task_time.elapsed()
+                );
+            }
+            if $task == -1 || $task == 2 {
+                let task_time = Instant::now();
+                println!(
+                    "Day {:>2}: Task 2: {:>16}, in {:?}",
+                    $i,
+                    paste::expr! {[<day $i>]::task2()},
+                    task_time.elapsed()
+                );
+            }
+        }
     };
+}
+
+fn main() {
+    let mut run_all = false;
+    let mut day = -1;
+    let mut task = -1;
+
+    let mut arg_iter = std::env::args();
+
+    // Discards the program name
+    arg_iter.next();
+
+    while let Some(arg) = arg_iter.next() {
+        match &arg[..] {
+            "-a" | "--all" => run_all = true,
+            "-d" | "--day" => {
+                day = arg_iter
+                    .next()
+                    .expect("Missing task number.")
+                    .parse()
+                    .expect("Invalid day number. Must be an integer between 1 and 25 (including).");
+                if !(1..=25).contains(&day) {
+                    panic!("Invalid day number. Must be an integer between 1 and 25 (including).");
+                }
+            }
+            "-t" | "--task" => {
+                task = arg_iter
+                    .next()
+                    .expect("Missing task number.")
+                    .parse()
+                    .expect("Invalid task number. Must be either 1 or 2.");
+                if !(1..=2).contains(&task) {
+                    panic!("Invalid task number. Must be either 1 or 2.");
+                }
+            }
+            _ => panic!("Invalid argument {}", arg),
+        }
+    }
+
+    let total_time = Instant::now();
+
+    run_day!(1, run_all, day, task);
+    run_day!(2, run_all, day, task);
+    run_day!(3, run_all, day, task);
+    run_day!(4, run_all, day, task);
+    run_day!(5, run_all, day, task);
+    run_day!(6, run_all, day, task);
+    run_day!(7, run_all, day, task);
+    run_day!(9, run_all, day, task);
+    run_day!(19, run_all, day, task);
+
+    if run_all {
+        println!("Total time elapsed: {:?}", total_time.elapsed());
+    }
 }
